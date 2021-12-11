9ab1e69c6b0e996fce7716b3b6aa90c054d3a54b
Nguyễn Văn Dũng <>
2021-12-11 16:03:55 +0700

    Defiy prices testnet

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -27,17 +27,26 @@ const farms: SerializedFarmConfig[] = [
     token: serializedTokens.defiy,
     quoteToken: serializedTokens.wbnb,
   },
-
-  // {
-  //   pid: 252,
-  //   lpSymbol: 'BUSD-BNB LP',
-  //   lpAddresses: {
-  //     97: '',
-  //     56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
-  //   },
-  //   token: serializedTokens.busd,
-  //   quoteToken: serializedTokens.wbnb,
-  // },
+  {
+    pid: 251,
+    lpSymbol: 'CAKE-BNB LP',
+    lpAddresses: {
+      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
+      56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
+    },
+    token: serializedTokens.cake,
+    quoteToken: serializedTokens.wbnb,
+  },
+  {
+    pid: 252,
+    lpSymbol: 'BUSD-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+    },
+    token: serializedTokens.busd,
+    quoteToken: serializedTokens.wbnb,
+  },
   /**
    * V3 by order of release (some may be out of PID order due to multiplier boost)
    */
