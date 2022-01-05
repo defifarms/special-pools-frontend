58f2ae6c4a1f3d1ba24895591bb852fe9ab24793
Nguyễn Văn Dũng <>
2022-01-06 02:32:47 +0700

    Fix UI card pool, farm

--- src/views/Farms/components/ToggleView/ToggleView.tsx
+++ src/views/Farms/components/ToggleView/ToggleView.tsx
@@ -15,6 +15,16 @@ const Container = styled.div`
     margin-left: 0;
   }
 `
+const WrapIcon = styled.div`
+  display: inline-block;
+  background-color: #6e2de5;
+  margin: 4px;
+  border-radius: 1px;
+  & > button {
+    width: 28px;
+    height: 27px;
+  }
+`
 
 const ToggleView: React.FunctionComponent<ToggleViewProps> = ({ viewMode, onToggle }) => {
   const handleToggle = (mode: ViewMode) => {
@@ -25,12 +35,16 @@ const ToggleView: React.FunctionComponent<ToggleViewProps> = ({ viewMode, onTogg
 
   return (
     <Container>
-      <IconButton variant="text" scale="sm" id="clickFarmCardView" onClick={() => handleToggle(ViewMode.CARD)}>
-        <CardViewIcon color={viewMode === ViewMode.CARD ? 'primary' : 'textDisabled'} />
-      </IconButton>
-      <IconButton variant="text" scale="sm" id="clickFarmTableView" onClick={() => handleToggle(ViewMode.TABLE)}>
-        <ListViewIcon color={viewMode === ViewMode.TABLE ? 'primary' : 'textDisabled'} />
-      </IconButton>
+      <WrapIcon>
+        <IconButton variant="text" scale="sm" id="clickFarmTableView" onClick={() => handleToggle(ViewMode.TABLE)}>
+          <ListViewIcon width={24} height={24} color={viewMode === ViewMode.TABLE ? 'cyan' : 'textDisabled'} />
+        </IconButton>
+      </WrapIcon>
+      <WrapIcon>
+        <IconButton variant="text" scale="sm" id="clickFarmCardView" onClick={() => handleToggle(ViewMode.CARD)}>
+          <CardViewIcon width={24} height={24} color={viewMode === ViewMode.CARD ? 'cyan' : 'textDisabled'} />
+        </IconButton>
+      </WrapIcon>
     </Container>
   )
 }
