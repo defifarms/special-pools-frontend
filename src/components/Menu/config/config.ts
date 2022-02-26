c9d856a5c0013c917f38151a0ed3beb1ffa385e8
Nguyễn Văn Dũng <>
2022-02-26 16:30:12 +0700

    Add claim

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -44,105 +44,91 @@ export type ConfigMenuItemsType = MenuItemsType & { hideSubNav?: boolean }
 const url = process.env.REACT_APP_HOST_1
 const specialUrl = process.env.REACT_APP_HOST_2
 
-const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
-  // {
-  //   label: t('Home'),
-  //   icon: 'HomeIcon',
-  //   href: `/`,
-  // },
-  {
-    label: t('Trade'),
-    icon: 'TradeIcon',
-    initialOpenState: true,
-    items: [
+const chainId = process.env.REACT_APP_CHAIN_ID
+
+const config: (t: ContextApi['t']) => MenuEntry[] = (t) => {
+  const configMenu = [
+    {
+      label: t('Trade'),
+      icon: 'TradeIcon',
+      initialOpenState: true,
+      items: [
+        {
+          label: t('Exchange'),
+          href: `/swap`,
+        },
+        {
+          label: t('Liquidity'),
+          href: `/liquidity`,
+        },
+      ],
+    },
+    {
+      label: t('Farms'),
+      icon: 'FarmIcon',
+      href: `/farms`,
+    },
+    {
+      label: t('Pools'),
+      icon: 'PoolIcon',
+      items: [
+        {
+          label: t('Classic Pools'),
+          href: `/pools`,
+        },
+        {
+          label: t('Special Pools'),
+          href: `/spool`,
+        },
+      ],
+    }
+  ]
+  if (chainId === '97') {
+    return [
       {
-        label: t('Exchange'),
-        href: `/swap`,
+        label: t('Trade'),
+        icon: 'TradeIcon',
+        initialOpenState: true,
+        items: [
+          {
+            label: t('Exchange'),
+            href: `/swap`,
+          },
+          {
+            label: t('Liquidity'),
+            href: `/liquidity`,
+          },
+        ],
       },
       {
-        label: t('Liquidity'),
-        href: `/liquidity`,
-      },
-    ],
-  },
-  {
-    label: t('Farms'),
-    icon: 'FarmIcon',
-    href: `/farms`,
-  },
-  {
-    label: t('Pools'),
-    icon: 'PoolIcon',
-    items: [
-      {
-        label: t('Classic Pools'),
-        href: `/pools`,
+        label: t('Farms'),
+        icon: 'FarmIcon',
+        href: `/farms`,
       },
       {
-        label: t('Special Pools'),
-        href: `/spool`,
-      },
-    ],
-  },
-  // {
-  //   label: t('Rocket'),
-  //   icon: 'RocketIcon',
-  //   href: `/rocket`,
-  //   status: {
-  //     text: "SOON",
-  //     color: "warning",
-  //   }
-  // },
-  // {
-  //   label: t('NFT'),
-  //   icon: 'NftIcon',
-  //   href: `/nft`,
-  //   status: {
-  //     text: "SOON",
-  //     color: "warning",
-  //   }
-  // },
-  // {
-  //   label: t('Lottery'),
-  //   icon: 'LotteryIcon',
-  //   href: `/lottery`,
-  //   status: {
-  //     text: "SOON",
-  //     color: "warning",
-  //   }
-  // },
-  // {
-  //   label: t('More'),
-  //   icon: 'MoreIcon',
-  //   initialOpenState: true,
-  //   items: [
-  //     {
-  //       label: t('Contact'),
-  //       href: 'https://docs.defifarms.org/contact-us',
-  //       target:"_blank"
-  //     },
-  //     {
-  //       label: t('Docs'),
-  //       href: 'https://docs.defifarms.org/',
-  //       target:"_blank"
-  //     },
-  //     {
-  //       label: 'Github',
-  //       href: 'https://github.com/defifarms',
-  //       target:"_blank"
-  //     },
-  //     {
-  //       label: 'Blog',
-  //       href: 'https://medium.com/@DefiFarmsNFTs',
-  //       target:"_blank"
-  //     },
-  //     {
-  //       label: 'Audited by',
-  //       href: 'https://callisto.network/defifarms-protocol-security-audit/',
-  //       target:"_blank"
-  //     },
-  //   ],
-  // },
-]
+        label: t('Pools'),
+        icon: 'PoolIcon',
+        items: [
+          {
+            label: t('Classic Pools'),
+            href: `/pools`,
+          },
+          {
+            label: t('Special Pools'),
+            href: `/spool`,
+          },
+        ],
+      }, {
+        label: t('Claim BNB'),
+        icon: 'NftIcon',
+        href: `https://testnet.binance.org/faucet-smart`,
+        target: "_blank"
+      }
+    ]
+  }
+  return configMenu
+}
+
+
 
 export default config
